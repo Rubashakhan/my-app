7546f97fa9988fb0cf8c8d6a210e35791912d347
Rubasha Khan <ruba.khanr.k465919@gmail.com>
2024-09-21 21:56:59 +0500

    this is an header and about page code

---
+++ app/component/About/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div >
+      <div>
+        <h1 >About</h1>
+        <ul>
+            <a href="">Home</a>
+            <a href="">Contact Us</a>
+        
+
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ app/component/Header/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Welcome to the page!</h1>
+    </div>
+  )
+}
+
+export default Header
